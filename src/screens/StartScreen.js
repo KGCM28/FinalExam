027e75355af5c479ca0e6b1c2b85dea5bb3123e1
KGCM28 <78344641+KGCM28@users.noreply.github.com>
2021-05-28 23:04:34 +0800

    Initial commit

---
+++ src/screens/StartScreen.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Paragraph from '../components/Paragraph'
+
+export default function StartScreen({ navigation }) {
+  return (
+    <Background>
+      <Logo />
+      <Header>Padala Express</Header>
+      <Paragraph>
+        Mabilis at Malinis na Serbisyo.
+      </Paragraph>
+      <Button
+        mode="contained"
+        onPress={() => navigation.navigate('LoginScreen')}
+      >
+        Login
+      </Button>
+      <Button
+        mode="outlined"
+        onPress={() => navigation.navigate('RegisterScreen')}
+      >
+        Sign Up
+      </Button>
+
+
+
+
+      
+    </Background>
+  )
+}
